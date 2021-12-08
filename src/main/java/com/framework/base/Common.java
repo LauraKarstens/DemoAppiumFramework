c7a780fc23c2ae51e32ad1ab19d37631c82730d8
madison-vincent <madison.vincent.work@gmail.com>
2021-12-08 10:33:36 -0700

    small changes

--- src/main/java/com/framework/base/Common.java
+++ src/main/java/com/framework/base/Common.java
@@ -1,6 +1,5 @@
 package com.framework.base;
 
-import com.pageObjects.AppPageObjects;
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.touch.WaitOptions;
 import io.appium.java_client.touch.offset.PointOption;
